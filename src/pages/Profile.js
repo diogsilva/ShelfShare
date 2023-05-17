f9533bd80ce0a125b90b3fbb9ceade6fab139136
João Diogo Craveiro Ferreira <jd.ferreira@ua.pt>
2023-05-18 00:25:47 +0100

    [WIP] pages/Profile.js: estrelas e contador sold/traded

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Grid, Avatar } from "@mui/material"
+import { Grid, Avatar, Divider, Rating } from "@mui/material"
 
 // Temporariamente só um user
 const antónio = require("../sample/users.json")[0];
-const profilePath = "/users/" + antónio.username + "/profile/"
-const avatarPath = profilePath + "avatar.jpg";
-const bannerPath = profilePath + "banner.jpg";
+const profilePath = "/profiles/" + antónio.username
+const avatarPath = profilePath + "/avatar.jpg";
+const bannerPath = profilePath + "/banner.jpg";
 
 console.log(antónio)
 console.log(avatarPath)
@@ -13,11 +13,22 @@ console.log(avatarPath)
 const Profile = (props) => {
 
 return (
+    <div>
     <Grid container className="section-body" spacing={5} style={{backgroundImage: "url('" + bannerPath + "')"}}>
         <Grid item xs={1} />
         <Grid item><Avatar src={avatarPath} /></Grid>
         <Grid item><p>{antónio.username}</p></Grid>
     </Grid>
+
+    <Grid container className="section-body" colums={3}>
+        <Grid item xs={1}><p className="profile-trade-counter">{antónio.booksSold}</p></Grid>
+        <Divider orientation="vertical" flexItem />
+        <Grid item xs={1}><p className="profile-trade-counter">{antónio.booksTraded}</p></Grid>
+        <Divider orientation="vertical" flexItem></Divider>
+    </Grid>
+
+    <Rating value={antónio.avgRating} precision={1/2} size="large" readOnly />
+    </div>
 )
 };
 
