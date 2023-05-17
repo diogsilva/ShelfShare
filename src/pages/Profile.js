bf6c3caa9858f876626aabcafeff0704c9bbc9c7
João Diogo Craveiro Ferreira <jd.ferreira@ua.pt>
2023-05-17 19:45:12 +0100

    [WIP] pages/Profile.js: Primeiro esqueleto

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,34 +1,15 @@
 import React from 'react';
+import { Grid } from "@mui/material"
 // Home Screen
 const Profile = (props) => {
 
-  return (
-    <div>
-      Profile
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-    </div>
-  )
+return (
+    <Grid container className="section-body" spacing={5}>
+        <Grid item sx={1} />
+        <Grid item><p>Avatar</p></Grid>
+        <Grid item><p>Username</p></Grid>
+    </Grid>
+)
 };
 
 export default Profile;
