0e346f370bc307b4f054793db6132ca1547a8a3c
João Diogo Craveiro Ferreira <jd.ferreira@ua.pt>
2023-05-17 23:32:58 +0100

    [WIP] pages/Profile.js: experimentar CSS

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Grid } from "@mui/material"
-// Home Screen
+import { Grid, Avatar } from "@mui/material"
+
+// Temporariamente só um user
+const antónio = require("../sample/users.json")[0];
+const profilePath = "/users/" + antónio.username + "/profile/"
+const avatarPath = profilePath + "avatar.jpg";
+const bannerPath = profilePath + "banner.jpg";
+
+console.log(antónio)
+console.log(avatarPath)
+
 const Profile = (props) => {
 
 return (
-    <Grid container className="section-body" spacing={5}>
-        <Grid item sx={1} />
-        <Grid item><p>Avatar</p></Grid>
-        <Grid item><p>Username</p></Grid>
+    <Grid container className="section-body" spacing={5} style={{backgroundImage: "url('" + bannerPath + "')"}}>
+        <Grid item xs={1} />
+        <Grid item><Avatar src={avatarPath} /></Grid>
+        <Grid item><p>{antónio.username}</p></Grid>
     </Grid>
 )
 };
