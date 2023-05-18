2a822f638d8bdaa1d655682a4dc53d9e6d7c5e1c
Miguel Moreira <83714496+moreiramiguel@users.noreply.github.com>
2023-05-18 02:35:53 +0100

    Profile Update
    Commented recent profile change

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,68 @@
-import React from 'react';
-import { Grid, Divider, Rating } from "@mui/material";
-import UserAvatar from '../Components/UserAvatar.js';
-import UserBanner from '../Components/UserBanner.js';
+import React, { useState } from 'react';
+import { Stack } from 'react-bootstrap';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import MyStats from '../Components/MyStats.jsx';
+import MyShelf from '../Components/MyShelf.jsx';
 
+
+const Profile = (props) => {
+
+    const [selectedTab,setSelectedTab] = useState();
+
+
+    const handleChange = (event, selectedTab) => {
+      setSelectedTab(selectedTab);
+    };
+
+  return (
+    <Stack spacing={2}>
+      <div>
+        <Avatar sx={{ marginTop: '8px', position: 'absolute', height: '125px', width: '125px' }} alt="Remy Sharp" src="https://i.pinimg.com/originals/78/ae/ae/78aeae1093015be662b0aa45a10a3174.jpg" />
+        <Box
+          sx={{
+            width: "auto",
+            height: 100,
+            backgroundColor: '#E0E0E0',
+            '&:hover': {
+              backgroundColor: 'primary.main',
+              opacity: [0.9, 0.8, 0.7],
+            },
+          }}
+        />
+        <Box
+          sx={{
+            width: "auto",
+            height: 60,
+            backgroundColor: '#E0E0E0',
+            '&:hover': {
+              backgroundColor: 'primary.main',
+              opacity: [0.9, 0.8, 0.7],
+            },
+          }}
+        />
+
+        <Tabs  value={selectedTab} onChange={handleChange} centered>
+        <Tab label="My Shelf" />
+        <Tab label="My Stats" />
+        <Tab label="My orders" disabled/>
+      </Tabs>
+      {selectedTab === 0 && <MyShelf />}
+      {selectedTab === 1 && <MyStats />}
+
+      </div>
+    </Stack>
+
+  )
+};
+
+export default Profile;
+
+
+
+/*
 // Temporariamente só um user
 const antónio = require("../sample/users.json")[0];
 const profilePath = "/profiles/" + antónio.userName
@@ -32,4 +92,5 @@ return (
 )
 };
 
-export default Profile;
+export default Profile; 
+*/
