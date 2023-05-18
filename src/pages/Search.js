5bbc9fe257aa690a17c43453795154d1b0bad7a4
João Diogo Craveiro Ferreira <jd.ferreira@ua.pt>
2023-05-18 04:07:32 +0100

    pages/Search.js: Implementar pesquisa de livros.
    Isto só pesquisa livros; a lista antiga foi removida.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -19,19 +19,44 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import SearchComponent from '../Components/SearchComponent';
 import { Stack } from '@mui/material';
+import BookInfoDialog from '../Components/BookInfoDialog.jsx';
 
+const bookData = require("../sample/books.json");
+var filteredBooks = bookData;
 
 // Search Screen
 
+function filterBooks(query) {
+  query = query.toLowerCase();
 
+  var foundTitles = []
+  var foundAuthors = []
+
+  bookData.map((book) => {
+    if (book.title.toLowerCase().includes(query))
+      foundTitles.push(book);
+
+    // if (book.author.includes(query))
+    //   foundAuthors +=
+  })
+  filteredBooks = foundTitles;
+  console.log("filtered books:", filteredBooks);
+}
 
 const Search = (props) => {
   const genres = ['Art', 'Biographies', 'Comics', 'Drama', 'Fiction'];
   const authors = ["Sally Ronney", "Kelly reid", "J.K Rowling", "Collen Hoover", "M.L.Rio"];
   const [value, setValue] = useState('');
-  const handleChange = event => setValue(event.target.value);
+  const handleChange = event => { setValue(event.target.value); filterBooks(value); }
   const [openAuthors, setOpenAuthors] = React.useState(true);
   const [openGenres, setOpenGenres] = React.useState(true);
+
+  const [bookInfo,setBookInfo] = useState({});
+  const [openBookInfo, setOpenBook] = useState(false);
+  const handleClickOpenBookInfo = (info) => {
+      setOpenBook(true);
+      setBookInfo(info);
+  };
 
   const handleClick = (val, setVal) => {
     setVal(!val);
@@ -53,7 +78,7 @@ const Search = (props) => {
         </Row>
       </Container>
 
-      <List
+      {/* <List
         sx={{minWidth:'70%'}}
         component="nav"
         aria-labelledby="nested-list-subheader"
@@ -87,8 +112,25 @@ const Search = (props) => {
             </ListItemButton>))}
           </List>
         </Collapse>
-      </List>
+      </List> */}
 
+
+      <List
+        id="results"
+        sx={{minWidth:'70%'}}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+      >
+        { filteredBooks.map((b) => {
+          const source = "/img/livros/" + b.imageLink;
+          return (<>
+            <BookInfoDialog key={"bookinfo " + b.title} bookInfo={bookInfo} open={openBookInfo} setOpen={setOpenBook} />
+            <ListItemButton key={"list " + b.title} onClick={()=>handleClickOpenBookInfo({...b, source})}>
+              <ListItemText>{b.title}</ListItemText>
+            </ListItemButton>
+          </>);
+        }) }
+      </List>
     </div>
   )
 };
