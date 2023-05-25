1cc15531fea47995494e4eff1efb21cca647f381
PatriciaMouraFerreira <84681445+PatriciaMouraFerreira@users.noreply.github.com>
2023-05-25 04:23:18 +0100

    Update MyStats.jsx

--- src/Components/MyStats.jsx
+++ src/Components/MyStats.jsx
@@ -1,50 +1,82 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import { Line } from 'react-chartjs-2';
+
 import {
   Chart as ChartJS,
   CategoryScale, // x axis
   LinearScale, // y axis
   PointElement,
   LineElement,
-  Legend,
+  Title,
+  Tooltip,
+  Legend
 } from 'chart.js';
-
 
 ChartJS.register(
   CategoryScale, // x axis
   LinearScale, // y axis
   PointElement,
   LineElement,
+  Title,
+  Tooltip,
   Legend
 );
-const data = {
+
+export const data = {
   labels: ['Jan', 'Ferb', 'Mar', 'Apr', 'May'],
   datasets: [{
-    label: 'Sales of the',
-    data: [3, 1, 8, 9, 7],
+    label: 'Sold',
+    data: [3, 1, 50, 9, 7],
+    backgroungColor: 'red',
+    borderColor: 'red',
+    pointBorderColor: 'red',
+    fill: true,
+    tension: 0.4,
+    backgroundColor: 'red',
+    yAxisID: 'y',
+  },{
+    label: 'Trade',
+    data: [40, 8, 10, 4, 20],
     backgroungColor: 'blue',
-    borderColor: 'black',
+    borderColor: 'blue',
     pointBorderColor: 'blue',
     fill: true,
-    tension: 0.4
+    tension: 0.4,
+    backgroundColor: 'blue',
+    yAxisID: 'y1',
   }]
 }
-const options = {
+
+export const options = {
+  responsive : true,
+  interaction: {
+    mode: 'index',
+    intersect: false,
+  },
+  stacked: false,
   plugins: {
-    legend: true
+    title: {
+      display: true,
+      text: 'My stats'
+    },
   },
   scales: {
     y: {
-      min: 0,
-      max: 70
-    }
-  }
-
+      type: 'linear',
+      display: true,
+      position: 'left'
+    },
+    y1: {
+      type: 'linear',
+      display: true,
+      position: 'right',
+      grid: {
+        drawOnChartArea: false,
+      },
+    },
+  },
 }
-
-
-
 
 const MyStats = () => {
   return (
@@ -90,91 +122,13 @@ const MyStats = () => {
           width: '30%'
         }}>Sold</h1>
       </Box>
-      <Box style={{ width: "80%", height: "60%", padingTop: "20%",paddingLeft:"20%",marginTop:"5%" }}>
+      <Box style={{ width: "80%", height: "60%", padingTop: "20%",paddingLeft:"20%",marginTop:"5%" , paddingBottom:"5%",color:"white"}}>
         <Line
           data={data}
           options={options}
 
         ></Line>
       </Box>
-
-
-      {/*  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-import faker from 'faker';
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
-export const options = {
-  responsive: true,
-  interaction: {
-    mode: 'index' as const,
-    intersect: false,
-  },
-  stacked: false,
-  plugins: {
-    title: {
-      display: true,
-      text: 'Chart.js Line Chart - Multi Axis',
-    },
-  },
-  scales: {
-    y: {
-      type: 'linear' as const,
-      display: true,
-      position: 'left' as const,
-    },
-    y1: {
-      type: 'linear' as const,
-      display: true,
-      position: 'right' as const,
-      grid: {
-        drawOnChartArea: false,
-      },
-    },
-  },
-};
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      yAxisID: 'y',
-    },
-    {
-      label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      yAxisID: 'y1',
-    },
-  ],
-};*/}
-
-
-
     </div>
 
   );
